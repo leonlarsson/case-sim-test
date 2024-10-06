98d67809af0ce8d81ec1798f17b4144619df8ac0
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-10-06 17:18:12 +0200

    dsdsa

--- db/index.ts
+++ db/index.ts
@@ -2,10 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema.ts";
 import postgres from "postgres";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is required");
-}
-
-export const pg = postgres(process.env.DATABASE_URL);
+export const pg = postgres(process.env.DATABASE_URL as string);
 
 export default drizzle(pg, { schema });

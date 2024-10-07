d6d98322d0d9d4b80672936e9b99f6c003790b5b
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-10-07 18:12:43 +0200

    test

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,10 @@
 import Database from "better-sqlite3";
 import * as schema from "./schema.ts";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import path from "path";
 
-export const sqlite = new Database("./sqlite/sqlite.db");
+console.log(path.resolve("/sqlite/sqlite.db"));
+
+export const sqlite = new Database(path.resolve("/sqlite/sqlite.db"));
 
 export default drizzle(sqlite, { schema });
